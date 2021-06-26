1bffa238d7f1f97660076d4571a6f69bd733c197
Donchi1 <64812693+Donchi1@users.noreply.github.com>
2021-06-26 12:08:15 +0400

    latest commit

--- src/components/navigation/contacts.js
+++ src/components/navigation/contacts.js
@@ -137,7 +137,7 @@ function Contacts() {
                     <div className="contact_detail">
                       {' '}
                       <span>WhatsApp</span>
-                      <p>+447459755944</p>
+                      <p>+447724648772</p>
                     </div>
                   </li>
                   <li
